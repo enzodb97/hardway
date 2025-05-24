0f8bf31c8ceec1220068022b74dae4bdec17dc05
Luciano Marelli <marelliluciano97@hotmail.com>
2025-05-24 16:17:20 -0300

    Se ajustan estilos en el formulario de registro de clientes: se reorganizan los campos 'Calle' y 'Altura', y se modifica el tipo de entrada de 'Teléfono' a numérico. Se corrigen estilos en Login.css para mejorar la presentación.

--- hardway/src/pages/AltaCliente/AltaCliente.tsx
+++ hardway/src/pages/AltaCliente/AltaCliente.tsx
@@ -195,14 +195,10 @@ const AltaCliente: React.FC = () => {
                   </IonSelect>
                 </IonItem>
 
-                {/* N° Documento */}
                 <IonItem className="form-item">
                   <IonLabel position="floating">N° Documento </IonLabel>
                   <IonInput
                     required
-                    type="number"
-                    inputmode="numeric"
-                    pattern="[0-9]*"
                     value={formData.numeroDocumento}
                     onIonChange={handleNumeroDocumentoChange}
                   />
@@ -241,40 +237,15 @@ const AltaCliente: React.FC = () => {
                   />
                 </IonItem>
 
-                {/* Calle y Altura en el mismo renglón */}
-                <div
-                  style={{
-                    display: "grid",
-                    gridTemplateColumns: "1fr 1fr",
-                    gap: "12px",
-                  }}
-                >
-                  <IonItem className="form-item" style={{ marginBottom: 0 }}>
-                    <IonLabel position="floating">Calle</IonLabel>
-                    <IonInput
-                      value={formData.calle}
-                      onIonChange={(e) =>
-                        setFormData({ ...formData, calle: e.detail.value! })
-                      }
-                    />
-                  </IonItem>
-                  <IonItem className="form-item" style={{ marginBottom: 0 }}>
-                    <IonLabel position="floating">Altura</IonLabel>
-                    <IonInput
-                      type="number"
-                      inputmode="numeric"
-                      pattern="[0-9]*"
-                      value={formData.altura}
-                      onIonChange={(e) => {
-                        const value = e.detail.value || "";
-                        if (/^\d*$/.test(value)) {
-                          setFormData({ ...formData, altura: value });
-                        }
-                        // Si no es número, no actualiza el valor
-                      }}
-                    />
-                  </IonItem>
-                </div>
+                <IonItem className="form-item">
+                  <IonLabel position="floating">Calle</IonLabel>
+                  <IonInput
+                    value={formData.calle}
+                    onIonChange={(e) =>
+                      setFormData({ ...formData, calle: e.detail.value! })
+                    }
+                  />
+                </IonItem>
 
                 <IonItem className="form-item">
                   <IonLabel position="floating">Piso</IonLabel>
@@ -315,6 +286,16 @@ const AltaCliente: React.FC = () => {
                 </IonItem>
 
                 <IonItem className="form-item">
+                  <IonLabel position="floating">Altura</IonLabel>
+                  <IonInput
+                    value={formData.altura}
+                    onIonChange={(e) =>
+                      setFormData({ ...formData, altura: e.detail.value! })
+                    }
+                  />
+                </IonItem>
+
+                <IonItem className="form-item">
                   <IonLabel position="floating">N° Departamento</IonLabel>
                   <IonInput
                     value={formData.numeroDepartamento}
@@ -338,14 +319,11 @@ const AltaCliente: React.FC = () => {
                   />
                 </IonItem>
 
-                {/* Teléfono */}
                 <IonItem className="form-item">
                   <IonLabel position="floating">Teléfono </IonLabel>
                   <IonInput
                     required
-                    type="number"
-                    inputmode="numeric"
-                    pattern="[0-9]*"
+                    type="tel"
                     value={formData.telefono}
                     onIonChange={handleTelefonoChange}
                   />
